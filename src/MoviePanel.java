d59eb5ec8a0f4893914d77a098b504a24b0373be
Jamie430wwwww <jamie430w@gmail.com>
2024-06-02 22:32:30 -0400

    Update MoviePanel.java
    update video player (incompleted)

--- src/MoviePanel.java
+++ src/MoviePanel.java
@@ -1,3 +1,5 @@
+package src;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -7,11 +9,38 @@ import java.awt.event.MouseListener;
 
 
 //Plan is to add images to the labels then make the buttons transparent
-public class MoviePanel implements MouseListener, ActionListener {
+public class MoviePanel implements MouseListener, ActionListener{
+
+
+
+    //film data
+    int cost = 10;
+    Movie YourName = new Movie("Your Name", cost, 2016, 107, Movie.Genres.Animation, "D:\\github program\\MovieTheatreExperience\\src\\Trailers\\YourName (2).mp4", "Unknown");
+    Movie MIB = new Movie("Mission impossible: fall out", cost, 2018, 147, Movie.Genres.Action, "D:\\github program\\MovieTheatreExperience\\src\\Trailers\\Mission_ Impossible - Fallout (2018) - Official Trailer - Paramount Pictures.mp4", "Unknown");
+    Movie StarWars = new Movie("Star Wars: Revenge of the Sith", cost, 2005, 140, Movie.Genres.Action, "D:\\github program\\MovieTheatreExperience\\src\\Trailers\\Star Wars_ Episode III - Revenge of the Sith (2005) Trailer #1 _ Movieclips Classic Trailers.mp4", "Unknown");
+    Movie EndGame = new Movie("Avengers: End Game", cost, 2019, 182, Movie.Genres.Action, "D:\\github program\\MovieTheatreExperience\\src\\Trailers\\Marvel Studios' Avengers - Official Trailer.mp4", "Unknown");
+    Movie IT = new Movie("It", cost, 2017, 135, Movie.Genres.Horror, "D:\\github program\\MovieTheatreExperience\\src\\Trailers\\It Trailer #1 (2017) _ Movieclips Trailers.mp4", "Unknown");
+    Movie AQP = new Movie("A Quiet Place", cost, 2018, 90, Movie.Genres.Horror, "D:\\github program\\MovieTheatreExperience\\src\\Trailers\\A QUIET PLACE _ Official Trailer _ Paramount Movies.mp4", "Unknown");
+    Movie Shining = new Movie("The Shining", cost, 1980, 136, Movie.Genres.Horror, "D:\\github program\\MovieTheatreExperience\\src\\Trailers\\shining.mp4", "Unknown");
+    Movie FSN = new Movie("Fate/stay night: Heaven's Feel I. presage flow", cost, 2017, 120, Movie.Genres.Animation, "D:\\github program\\MovieTheatreExperience\\src\\Trailers\\FSNHF1.mp4", "Unknown");
+    Movie Suzume = new Movie("Suzume", cost, 2022, 122, Movie.Genres.Animation, "D:\\github program\\MovieTheatreExperience\\src\\Trailers\\Suzume.mp4", "Unknown");
+
+
+
+
+
+
     JFrame frame;
     JLabel bGlabel, title, selectGenre;
-    JButton backButton, button1, button2, button3, genre1, genre2, genre3;
+    JButton backButton, button1, button2, button3, button4, button5, button6, button7, button8, button9,  genre1, genre2, genre3;
     int page;
+
+
+
+    public static void main(String [] args){
+        new MoviePanel();
+    }
+
 
     public MoviePanel() {
         setPage();
@@ -21,7 +50,10 @@ public class MoviePanel implements MouseListener, ActionListener {
         frame = new JFrame();
         frame.setSize(1280, 800);
         frame.setLocationRelativeTo(null);
-        bGlabel = new JLabel(new ImageIcon("Res/brickwall.jpg"));
+
+
+
+        bGlabel = new JLabel(new ImageIcon(getClass().getResource("brickwall.jpg")));
         bGlabel.setBounds(0, 0, 1280, 800);
         frame.add(bGlabel);
 
@@ -97,6 +129,7 @@ public class MoviePanel implements MouseListener, ActionListener {
             switch (textButton) {
                 case "Back":
                     frame.dispose();
+
                     if (page == 1){
                         setPage();
                     }
@@ -110,15 +143,15 @@ public class MoviePanel implements MouseListener, ActionListener {
                     bGlabel.revalidate();
                     bGlabel.repaint();
                     title.setText(genre2.getText());
-                    button1 = new JButton(new ImageIcon("./Res/It.jpg"));
+                    button1 = new JButton(new ImageIcon(getClass().getResource("It.jpg")));
                     button1.setBounds(69, 150, 335, 500);
                     button1.addActionListener(this);
                     bGlabel.add(button1);
-                    button2 = new JButton(new ImageIcon("./Res/AQuietPlace.jpg"));
+                    button2 = new JButton(new ImageIcon(getClass().getResource("AQuietPlace.jpg")));
                     button2.setBounds(473, 150, 335, 500);
                     button2.addActionListener(this);
                     bGlabel.add(button2);
-                    button3 = new JButton(new ImageIcon("./Res/Shining.jpg"));
+                    button3 = new JButton(new ImageIcon(getClass().getResource("Shining.jpg")));
                     button3.setBounds(877, 150, 335, 500);
                     button3.addActionListener(this);
                     bGlabel.add(button3);
@@ -132,18 +165,18 @@ public class MoviePanel implements MouseListener, ActionListener {
                     bGlabel.revalidate();
                     bGlabel.repaint();
                     title.setText(genre1.getText());
-                    button1 = new JButton(new ImageIcon("./Res/MissionImp.jpg"));
-                    button1.setBounds(69, 150, 335, 500);
-                    button1.addActionListener(this);
-                    bGlabel.add(button1);
-                    button2 = new JButton(new ImageIcon("./Res/Starwars.jpg"));
-                    button2.setBounds(473, 150, 335, 500);
-                    button2.addActionListener(this);
-                    bGlabel.add(button2);
-                    button3 = new JButton(new ImageIcon("./Res/Avengers.jpg"));
-                    button3.setBounds(877, 150, 335, 500);
-                    button3.addActionListener(this);
-                    bGlabel.add(button3);
+                    button4 = new JButton(new ImageIcon(getClass().getResource("MissionImp.jpg")));
+                    button4.setBounds(69, 150, 335, 500);
+                    button4.addActionListener(this);
+                    bGlabel.add(button4);
+                    button5 = new JButton(new ImageIcon(getClass().getResource("Starwars.jpg")));
+                    button5.setBounds(473, 150, 335, 500);
+                    button5.addActionListener(this);
+                    bGlabel.add(button5);
+                    button6 = new JButton(new ImageIcon(getClass().getResource("Avengers.jpg")));
+                    button6.setBounds(877, 150, 335, 500);
+                    button6.addActionListener(this);
+                    bGlabel.add(button6);
                     break;
                 case "Animation":
                     page = 1;
@@ -154,20 +187,96 @@ public class MoviePanel implements MouseListener, ActionListener {
                     bGlabel.revalidate();
                     bGlabel.repaint();
                     title.setText(genre3.getText());
-                    button1 = new JButton(new ImageIcon("./Res/YourName.jpg"));
-                    button1.setBounds(69, 150, 335, 500);
-                    button1.addActionListener(this);
-                    bGlabel.add(button1);
-                    button2 = new JButton(new ImageIcon("./Res/FateNight.jpg"));
-                    button2.setBounds(473, 150, 335, 500);
-                    button2.addActionListener(this);
-                    bGlabel.add(button2);
-                    button3 = new JButton(new ImageIcon("./Res/Suzume.jpg"));
-                    button3.setBounds(877, 150, 335, 500);
-                    button3.addActionListener(this);
-                    bGlabel.add(button3);
-                    break;
+                    button7 = new JButton(new ImageIcon(getClass().getResource("YourName.jpg")));
+                    button7.setBounds(69, 150, 335, 500);
+                    button7.addActionListener(this);
+                    bGlabel.add(button7);
+                    button8 = new JButton(new ImageIcon(getClass().getResource("FateNight.jpg")));
+                    button8.setBounds(473, 150, 335, 500);
+                    button8.addActionListener(this);
+                    bGlabel.add(button8);
+                    button9 = new JButton(new ImageIcon(getClass().getResource("Suzume.jpg")));
+                    button9.setBounds(877, 150, 335, 500);
+                    button9.addActionListener(this);
+                    bGlabel.add(button9);
+                    System.out.println(111);
+
             }
+        }
+
+
+        if (e.getSource() == button1){
+            System.out.println(101);
+            frame.add(IT.createTrailerPlayer(), BorderLayout.CENTER);
+
+            button1.setVisible(false);
+            button2.setVisible(false);
+            button3.setVisible(false);
+        }
+        if (e.getSource() == button2){
+            System.out.println(101);
+            frame.add(AQP.createTrailerPlayer(), BorderLayout.CENTER);
+
+            button1.setVisible(false);
+            button2.setVisible(false);
+            button3.setVisible(false);
+        }
+        if (e.getSource() == button3){
+            System.out.println(101);
+            frame.add(Shining.createTrailerPlayer(), BorderLayout.CENTER);
+
+            button1.setVisible(false);
+            button2.setVisible(false);
+            button3.setVisible(false);
+        }
+        if (e.getSource() == button4){
+            System.out.println(101);
+            frame.add(MIB.createTrailerPlayer(), BorderLayout.CENTER);
+
+            button4.setVisible(false);
+            button5.setVisible(false);
+            button6.setVisible(false);
+        }
+        if (e.getSource() == button5){
+            System.out.println(101);
+            frame.add(StarWars.createTrailerPlayer(), BorderLayout.CENTER);
+
+            button4.setVisible(false);
+            button5.setVisible(false);
+            button6.setVisible(false);
+        }
+        if (e.getSource() == button6){
+            System.out.println(101);
+            frame.add(EndGame.createTrailerPlayer(), BorderLayout.CENTER);
+
+            button4.setVisible(false);
+            button5.setVisible(false);
+            button6.setVisible(false);
+        }
+        if (e.getSource() == button7){
+            System.out.println(101);
+            frame.add(YourName.createTrailerPlayer(), BorderLayout.CENTER);
+
+            button7.setVisible(false);
+            button8.setVisible(false);
+            button9.setVisible(false);
+        }
+        if (e.getSource() == button8){
+            System.out.println(101);
+            frame.add(FSN.createTrailerPlayer(), BorderLayout.CENTER);
+            frame.setVisible(true);
+
+            button7.setVisible(false);
+            button8.setVisible(false);
+            button9.setVisible(false);
+        }
+        if (e.getSource() == button9){
+            System.out.println(101);
+            frame.add(Suzume.createTrailerPlayer(), BorderLayout.CENTER);
+
+            button7.setVisible(false);
+            button8.setVisible(false);
+            button9.setVisible(false);
         }
     }
 }
