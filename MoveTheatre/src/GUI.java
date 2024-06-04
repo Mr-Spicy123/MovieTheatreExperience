8408d9c078d5328f3a0edd0c936b03228cd74b31
chairzihan <146217784+chairzihan@users.noreply.github.com>
2024-06-03 20:21:59 -0400

    name changes for class

---
+++ MoveTheatre/src/GUI.java
@@ -0,0 +1,26 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class GUI extends JFrame {
+	
+	JFrame frame;
+	
+	
+	public GUI() {
+		
+		
+		
+		frame = new JFrame("Snake");
+		frame.add(new SnakeGamePanel());
+		
+
+		frame.setResizable(false);
+		frame.pack();
+		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		frame.setLocationRelativeTo(null);
+		
+		
+	}
+
+}
